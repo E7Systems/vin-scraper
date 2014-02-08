1ad31cf0974d77b8ffd292f2b7b4a9fafec65f8d
Dennis Suratna <dennis.suratna@gmail.com>
2014-02-08 12:16:27 -0800

    Extracting database name and table name to const

--- vin_scraper.rb
+++ vin_scraper.rb
@@ -6,10 +6,12 @@ class VinScraper
   YEARS = 1953..2014
   INCLUDED_MAKES = {"acura" => true, "aston martin" => true, "audi" => true, "bentley" => true, "bmw" => true, "buick" => true, "cadillac" => true, "chevrolet" => true, "chrysler" => true, "daewoo" => true, "dodge" => true, "ford" => true, "gmc" => true, "infiniti" => true, "international" => true, "isuzu" => true, "jaguar" => true, "jeep" => true, "kia" => true, "lamborghini" => true, "land rover" => true, "lexus" => true, "lincoln" => true, "lotus" => true, "maserati" => true, "maybach" => true, "mazda" => true, "mercedes-benz" => true, "mercury" => true, "mini" => true, "mitsubishi" => true, "nissan" => true, "pontiac" => true, "porsche" => true, "rolls-royce" => true, "saab" => true, "saturn" => true, "scion" => true, "sterling" => true, "subaru" => true, "suzuki" => true, "toyota" => true, "triumph" => true, "volkswagen" => true, "volvo" => true}
   CONTAINER_CSS_PATH = 'div.mainframe h1.page-title+div table a'
+  DATABASE_NAME = 'vin_data'
+  TABLE_NAME = 'vins'
 
   def initialize(debug=false)
     @debug = debug
-    @db = Mysql2::Client.new(:host => 'localhost', :username => 'root', database: 'vin_data')
+    @db = Mysql2::Client.new(:host => 'localhost', :username => 'root', database: DATABASE_NAME)
     @agent = Mechanize.new
   end
 
@@ -30,8 +32,6 @@ class VinScraper
               next if first_model_child.nil?
               model_name = first_model_child.text
 
-              puts "#{year} #{make_name} #{model_name}" if @debug
-
               @agent.get("#{ROOT_URL}/#{year}/#{make_name}/#{model_name}") do |vins_page|
                 vins_page.search(CONTAINER_CSS_PATH).each do |vin_anchor|
                   vin_anchor_child = vin_anchor.children.first
@@ -51,7 +51,7 @@ class VinScraper
   private
   def insert_vin_record(year, make, model, vin)
     insert_query = <<-eos
-      INSERT INTO vins
+      INSERT INTO #{TABLE_NAME}
       (year, make, model, vin)
       VALUES
       (#{year.to_i}, '#{make.downcase}', '#{model.downcase}', '#{vin.downcase}')
